bfb5bf8d91eef61f7a945080c1886aeb036d40ca
Victoria  Topham <topham.victoria@gmail.com>
2020-04-22 10:12:49 -0700

    history pushes to the movie page now, but it sometimes happens before the state is upated so u don't see ur edits

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -39,14 +39,15 @@ const UpdateMovie = props => {
 
     const handleSubmit = event => {
         event.preventDefault()
+        setMovie(initialMovie)
         axios
             .put(`http://localhost:5000/api/movies/${params.id}`,movie)
             .then(
-                setMovie(initialMovie),
-                history.push("/movies"))
+                history.push(`/movies/${params.id}`))
             .catch(err => console.log(err))
-
+        
     }
+
 
 
     
